ea84abfd3225996aa098ec7a2fe37e21d94e725e
Geoff Jamieson <geoff.ijamieson@gmail.com>
2021-10-22 08:23:41 -0400

    changes to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
+import { faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
 import './App.scss';
 
+let quoteDB = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+
 const facebookIcon = <FontAwesomeIcon icon={faFacebookSquare} />
+const twitterIcon = <FontAwesomeIcon icon={faTwitterSquare} />
 
 function App() {
-  const [quote, newQuote] = useState("An unexamined life is not worth living.")
-  const [author, newAuthor] = useState("Socrates")
 
-  const changeQuoteAndAuthor = () => {
-    newQuote("If you want to lift yourself up, lift up someone else.");
-    newAuthor("Booker T. Washington");
-  }
+  const [quote, setQuote] = useState("Strive not to be a success, but rather to be of value.")
+  const [author, setAuthor] = useState("Albert Einstein")
   
   return (
     <div className="App">
@@ -25,14 +25,18 @@ function App() {
             <p id="author">- {author}</p>
           </div>
           <div id="quote-button-box">
-            <button id="new-quote" onClick={changeQuoteAndAuthor}>New Quote</button>
+            <button id="new-quote" onClick={() => {
+              setQuote("We become what we think about.")
+              ;
+              setAuthor("Earl Nightingale")
+            }}>New Quote</button>
           </div>
-          <div className="social">
+          <div className="social-icons">
             <div id="twitter-icon">
-              <a href="twitter.com/intent/tweet" id="tweet-quote"></a>
+              <a href="twitter.com/intent/tweet" id="tweet-quote">{twitterIcon}</a>
             </div>
             <div id="facebook-icon">
-              <a href="" id="facebook-quote">{facebookIcon}</a>
+              <a href="facebook.com" id="facebook-quote">{facebookIcon}</a>
             </div>
           </div>
         </div>
