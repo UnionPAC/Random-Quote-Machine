b25cbfcda6058b7f19fcb8126c1b949184e0e5d2
Geoff Jamieson <geoff.ijamieson@gmail.com>
2021-10-22 13:39:28 -0400

    made final changes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import { faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
 import './App.scss';
 import COLOURS_ARRAY from "./coloursArray";
 
-let quoteDB = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
-
 const facebookIcon = <FontAwesomeIcon icon={faFacebookSquare} id="fbIcon"/>
 const twitterIcon = <FontAwesomeIcon icon={faTwitterSquare} id="twitterIcon" />
 
